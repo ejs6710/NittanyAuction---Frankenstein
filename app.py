035fa0565a29bdbb6decfb7013571eb16e5343aa
ethansiu <ejs6710@psu.edu>
2026-04-11 17:22:06 -0400

    Added UserProfile, personalInformation, and Home pages as well as finished login for users not part of a role

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+import email
+import os
+
+from flask import Flask, render_template, request, session
 import sqlite3
 import hashlib
-
+import random
+import string
 app = Flask(__name__)
 
 HOST = 'http://127.0.0.1:5000/'
-DB_NAME = 'database.db'
+DB_NAME = 'identifier.sqlite'
 
 def connect():
   return sqlite3.connect(DB_NAME)
@@ -13,6 +17,7 @@ def connect():
 def hash_password(password):
   return hashlib.sha256(password.encode()).hexdigest()
 
+app.secret_key = hash_password("12314214151241")
 @app.route('/')
 def index():
 	return render_template('login.html')
@@ -21,31 +26,35 @@ def index():
 def login():
 	error = None
 	if request.method == 'POST':
-		email, password = request.form['email'], request.form['password']
+		session['email'] = request.form['email']
+		session['password'] =  request.form['password']
 
 		connection = connect()
-		cursor = connection.execute("SELECT * FROM Users WHERE email = ? AND password = ?", (email, hash_password(password)))
+		cursor = connection.execute("SELECT * FROM Users WHERE email = ? AND password = ?", (session.get('email'), hash_password(session.get('password'))))
 		user = cursor.fetchone()
 		connection.close()
 
 		if user:
 			connection = connect()
 
-			if connection.execute("SELECT * FROM Helpdesk WHERE email = ?", (email,)).fetchone():
+			if connection.execute("SELECT * FROM Helpdesk WHERE email = ?", (session.get('email'),)).fetchone():
+				session['role'] = 'Helpdesk'
 				connection.close()
-				return render_template('helpdesk.html', email=email)
+				return render_template('UserProfile.html', email=session.get('email'), role =session.get('role'))
 
-			if connection.execute("SELECT * FROM Bidders WHERE email = ?", (email,)).fetchone():
+			if connection.execute("SELECT * FROM Bidders WHERE email = ?", (session.get('email'),)).fetchone():
+				session['role'] = 'Bidders'
 				connection.close()
-				return render_template('bidders.html', email=email)
+				return render_template('UserProfile.html', email=session.get('email'), role =session.get('role'))
 
-			if connection.execute("SELECT * FROM Sellers WHERE email = ?", (email,)).fetchone():
+			if connection.execute("SELECT * FROM Sellers WHERE email = ?", (session.get('email'),)).fetchone():
+				session['role'] = 'Sellers'
 				connection.close()
-				return render_template('sellers.html', email=email)
+				return render_template('UserProfile.html', email=session.get('email'), role =session.get('role'))
 
-			if connection.execute("SELECT * FROM Users WHERE email = ?", (email,)).fetchone():
+			if connection.execute("SELECT * FROM Users WHERE email = ?", (session.get('email'),)).fetchone():
 				connection.close()
-				return render_template('users.html', email=email)
+				return render_template('users.html', email=session.get('email'))
 			
 			connection.close()
 			error = "Unexpected error"
@@ -53,6 +62,48 @@ def login():
 		else:
 			error = "Invalid email or password"
 	return render_template('login.html', error=error)
+
+@app.route(rule="/UserProfile", methods=["GET", "POST"])
+def user_profile(): #Reroutes to UserProfile
+	error = None
+	return render_template('UserProfile.html', email=session.get('email'), role = session.get('role'))
+
+@app.route(rule='/Home', methods=['GET', 'POST'])
+def home(): #Reroutes to home page
+	error = None
+	return render_template('Home.html', error=error)
+
+@app.route(rule='/Personal_Information', methods=['GET', 'POST'])
+def personal_information(): #Reroutes to personal information update page
+	error = None
+	return render_template('Personal_Information.html', error=error)
+
+@app.route(rule='/Update_Info', methods=['GET', 'POST'])
+def Update_Information(): #Updates information for users not part of any role
+	#Automatically adds them to bidders table
+	error = None
+	connection = connect()
+	if request.method == 'POST':
+		#Retrieves all values
+		major = request.form['Major']
+		age = request.form['Age']
+		lastname = request.form['Last_Name']
+		firstname = request.form['First_Name']
+		zipcode = request.form['Zipcode']
+		streetnum = request.form['Street_Number']
+		streetname = request.form['Street_Name']
+		address_id = "" #Generates a random 40 character address id
+		for i in range(0, 20):
+			address_id += str(random.randint(0, 9))
+			address_id += random.choice(string.ascii_letters)
+		#Inserts the address and user into the tables
+		connection.execute("INSERT INTO Address (address_id, zipcode, street_num, street_name) VALUES (?,?,?,?)", (address_id, zipcode, streetnum, streetname))
+		connection.execute("INSERT INTO Bidders (email, first_name, last_name, age, home_address_id, major) VALUES (?,?,?,?,?,?)", (session.get("email"), firstname, lastname, age, address_id, major))
+		connection.commit()
+	connection.close()
+	#Reroutes to the UserProfile table
+	return render_template('UserProfile.html', email=session.get('email'), role = session.get('Bidder'))
+
 
 if __name__ == "__main__":
 	app.run(debug=True, port=5000)
