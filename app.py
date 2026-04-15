452eded08b6743002f4a39150523048d9961f9e6
Qistina Zamzuri <qishieeeee@gmail.com>
2026-04-15 12:37:13 -0400

    Implemented register page for new user

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import email
 import os
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 import hashlib
 import random
@@ -63,6 +63,48 @@ def login():
 			error = "Invalid email or password"
 	return render_template('login.html', error=error)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register(): # user registration
+    error  = None
+    if request.method  == 'POST':
+
+        email =  request.form['email'].strip()
+        password = request.form['password']
+        confirm =  request.form['confirm_password']
+ 
+        if password != confirm:
+            error = "Passwords do not match."
+            return render_template('register.html', error=error)
+
+		
+        connection = connect()
+		# check if input email already exist in User table
+        existing  = connection.execute(
+            "SELECT 1 FROM Users WHERE email = ?", (email,)
+        ).fetchone()
+
+		# email registered, reject registration
+        if existing:
+            connection.close()
+            error  = f"{email} account already exists."
+            return render_template('register.html', error=error)
+
+		# email is unique, accept registration with hashed password
+        connection.execute(
+            "INSERT INTO Users (email, password) VALUES (?, ?)",
+            (email, hash_password(password))
+        )
+        connection.commit()
+        connection.close()
+
+
+        session['email'] = email
+        session['role'] =  None # No role yet
+
+        return render_template('UserProfile.html', email=session.get('email'), role = session.get(None))
+ 
+    return render_template('register.html', error=error)
+ 
 @app.route(rule="/UserProfile", methods=["GET", "POST"])
 def user_profile(): #Reroutes to UserProfile
 	error = None
